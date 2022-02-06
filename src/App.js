4df6cf448c1f9aa0b315cd2ae0e4ff41b4fea2e6
annabond <annabond@bondpmac.bondp.home>
2022-02-05 22:22:25 -0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './Data';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+const [place, setPlace]= useState(data)
+console.log(place);
+
+const removePlace=(id)=>{
+  let newArray= place.filter(item=> item.id !== id);
+  setPlace(newArray);
+}
+const [i, setPic]= useState(0);
+const [info, setInfo]= useState(data);
+let newArr= [...info];
+
+const nextPic=(e,index)=>{
+  setInfo(newArr);
+  const photo= document.querySelectorAll('#photo');
+  newArr[index]= place[index];
+
+  setPic((i=>{
+    i++;
+    if (i>info.length-1){
+      i=0;
+    }
+    return i;
+  }))
+  photo[index].setAttribute('src', newArr[index].image[i])
+}
+const previousPic=(e,index)=>{
+  setInfo(newArr);
+  const photo= document.querySelectorAll('#photo');
+  newArr[index]= place[index];
+
+  setPic((i=>{
+    i --;
+    if(i<0){
+      i=info.length-1;
+    }
+    return i;
+  }))
+  photo[index].setAttribute('src', newArr[index].image[i])
+}
+
+
+return(
+  <div>
+    <div>
+      <h1>Top {place.length} places I want to visit</h1>
     </div>
-  );
+    {place.map((x=>{
+      const {id, name, image}= x;
+      const index= place.indexOf(x);
+      return(
+        <div key={id}>
+          <h2>{id}. {name}</h2>
+          <img id="photo" src={image[0]} width="300" alt="place"/>
+          <div>
+          <button onClick={ (e)=>previousPic(e,index)}>Previous</button>
+          <button onClick={(e)=>nextPic(e, index)}>Next</button>
+          </div>
+          <div>
+          <button onClick={()=>removePlace(id)}>Remove place</button>
+          </div>
+        </div>
+      )
+    }))}
+
+    <div>
+    <button onClick={()=>setPlace([])}>Remove All</button>
+    </div>
+   
+  </div>
+)
 }
 
 export default App;
---
+++ src/Data.js
@@ -0,0 +1,26 @@
+import hawaii1 from './HAWAII1.jpeg';
+import hawaii2 from './hawaii2.jpeg';
+import hawaii3 from './hawaii3.jpeg';
+import tofino1 from './tofino1.jpeg';
+import tofino2 from './tofino2.jpeg';
+import tofino3 from './tofino3.jpeg';
+import whistler1 from './whistler1.jpeg';
+import whistler2 from './whistler2.jpeg';
+import whistler3 from './whistler3.jpeg';
+export const data = [
+    {
+        id:1,
+        name:'Hawaii',
+        image:[hawaii1, hawaii2, hawaii3]
+    },
+    {
+        id:2,
+        name:'Tofino',
+        image:[tofino1, tofino2, tofino3]
+    },
+    {
+        id:3,
+        name:'Whistler',
+        image:[whistler1, whistler2, whistler3]
+    }
+]
